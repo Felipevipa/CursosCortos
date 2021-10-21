a0b28fd8f1dd4a1d731c7847bddf9ff22ed035ff
felipevipa <felipevipa@outlook.com>
2021-10-21 16:33:53 -0500

    Modelos Creados

--- cursos_rapidos/nombre/admin.py
+++ cursos_rapidos/nombre/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Estudiante, Carrera
+from .models import Estudiante, Carrera, Calificacion, Tematica, Quiz, Material, Pregunta, OpcionRespuesta, Respuesta, Materia, Docente
 # Register your models here.
 
 admin.site.register(Estudiante)
 admin.site.register(Carrera)
+admin.site.register(Calificacion)
+admin.site.register(Tematica)
+admin.site.register(Quiz)
+admin.site.register(Material)
+admin.site.register(Pregunta)
+admin.site.register(OpcionRespuesta)
+admin.site.register(Respuesta)
+admin.site.register(Materia)
+admin.site.register(Docente)
--- cursos_rapidos/nombre/models.py
+++ cursos_rapidos/nombre/models.py
@@ -1,16 +1,92 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 #'id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'))
 
 
+
 class Carrera(models.Model):
     nombre      = models.CharField(max_length=50)
 
+    def __str__(self):
+        return '%s' % self.nombre
+
 
 class Estudiante(models.Model):
+    nombres     = models.CharField(max_length=40)
+    apellidos   = models.CharField(max_length=40)
+    documento   = models.IntegerField()
+    fechadenacimiento = models.DateField()
+    email       = models.EmailField()
     usuario     = models.CharField(max_length=50)
     contrasena  = models.CharField(max_length=18)
     carrera     = models.ForeignKey(Carrera, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return '%s %s' % (self.nombres, self.apellidos)
 
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=40)
+    carrera = models.ForeignKey(Carrera, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.nombre, self.carrera)
+
+
+class Docente(models.Model):
+    nombres = models.CharField(max_length=40)
+    apellidos = models.CharField(max_length=40)
+    documento = models.IntegerField()
+    fechadenacimiento = models.DateField()
+    email = models.EmailField()
+    usuario = models.CharField(max_length=50)
+    contrasena = models.CharField(max_length=18)
+    carrera = models.ForeignKey(Carrera, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.nombres, self.apellidos)
+
+
+class Tematica(models.Model):
+    id          = models.BigAutoField(primary_key=True, serialize=False)
+    titulo      = models.CharField(max_length=40)
+    materia     = models.ForeignKey(Materia, on_delete=models.SET_NULL, null=True)
+    docente     = models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True)
+
+
+class Material(models.Model):
+    contenido_de_texto = models.TextField(null=True)
+    imagen             = models.ImageField(null=True)
+    video              = models.URLField(null=True)
+    materialExterno    = models.URLField(null=True)
+    tematica           = models.ForeignKey(Tematica, on_delete=models.SET_NULL, null=True)
+
+
+class Quiz(models.Model):
+    tematica    = models.ForeignKey(Tematica, on_delete=models.SET_NULL, null=True)
+
+
+class Calificacion(models.Model):
+    fecha       = models.DateTimeField()
+    tiempo      = models.TimeField()
+    nota        = models.DecimalField(max_digits=2, decimal_places=2)
+    quiz        = models.ForeignKey(Quiz, on_delete=models.SET_NULL, null=True)
+    estudiante  = models.ForeignKey(Estudiante, on_delete=models.SET_NULL, null=True)
+
+
+class Pregunta(models.Model):
+    enunciado   = models.TextField()
+    quiz        = models.ForeignKey(Quiz, on_delete=models.SET_NULL, null=True)
+
+
+class OpcionRespuesta(models.Model):
+    texto_corto   = models.CharField(max_length=100, null=True)
+    texto_largo   = models.TextField()
+    opcion_logica = models.BooleanField(default=False)
+    pregunta      = models.ForeignKey(Pregunta, on_delete=models.SET_NULL, null=True)
+
+
+class Respuesta(models.Model):
+    respuesta_correcta = models.ForeignKey(OpcionRespuesta, on_delete=models.SET_NULL, null=True)
